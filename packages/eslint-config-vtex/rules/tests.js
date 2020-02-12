98519951fc417fea5e725d41c5f8c28a5fdd0c5c
Christian Kaisermann <christian@kaisermann.me>
2020-02-12 13:46:00 -0300

    Turn off consitent-test-it

--- packages/eslint-config-vtex/rules/tests.js
+++ packages/eslint-config-vtex/rules/tests.js
@@ -23,7 +23,7 @@ module.exports = {
         // Enforce consistent a test method name
         // https://github.com/jest-community/eslint-plugin-jest/blob/master/docs/rules/consistent-test-it.md
         'jest/consistent-test-it': [
-          'error',
+          'off',
           {
             fn: 'it',
             withinDescribe: 'it',
