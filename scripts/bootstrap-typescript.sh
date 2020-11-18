be0338393572b509c7361eb27070ac1aea7ab026
Christian Kaisermann <christian@kaisermann.me>
2020-11-18 12:04:27 -0300

    Remove unneeded direct dep from script

--- scripts/bootstrap-typescript.sh
+++ scripts/bootstrap-typescript.sh
@@ -1,4 +1,4 @@
-yarn add -D eslint prettier eslint-config-vtex eslint-plugin-vtex @vtex/prettier-config typescript @vtex/tsconfig
+yarn add -D eslint prettier eslint-config-vtex @vtex/prettier-config typescript @vtex/tsconfig
 echo '{ "extends": "vtex", "plugins": ["vtex"] }' >> .eslintrc
 echo '"@vtex/prettier-config"' >> .prettierrc
 echo '{ "extends": "@vtex/tsconfig" }' >> tsconfig.json
