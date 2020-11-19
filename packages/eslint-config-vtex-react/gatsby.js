022daf4bb4057c4b81235a1e13040f889c837abb
Christian Kaisermann <christian@kaisermann.me>
2020-11-19 12:37:19 -0300

    Ignore jsx-filename rule for gatsby-browser

--- packages/eslint-config-vtex-react/gatsby.js
+++ packages/eslint-config-vtex-react/gatsby.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['./index.js'],
   overrides: [
+    // gatsby config files
     {
       files: ['gatsby-{ssr,browser,node,config}.{js,jsx,ts,tsx}'],
       rules: {
@@ -9,10 +10,18 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
+    // allowed to use node libs
     {
       files: ['gatsby-{node,config,ssr}.{js,jsx,ts,tsx}'],
       rules: {
         'import/no-nodejs-modules': 'off',
+      },
+    },
+    // needs to be .js/.js
+    {
+      files: ['gatsby-browser.{js,ts}'],
+      rules: {
+        'react/jsx-filename-extension': 'off',
       },
     },
   ],
