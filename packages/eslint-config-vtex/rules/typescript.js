f7ba81f530973f6f2649c98bcc2521c3fc8d8a0f
Christian Kaisermann <christian@kaisermann.me>
2020-12-03 16:41:55 -0300

    Enable ts version of no-shadow rule

--- packages/eslint-config-vtex/rules/typescript.js
+++ packages/eslint-config-vtex/rules/typescript.js
@@ -22,7 +22,6 @@ module.exports = !hasTypescript
               // look in the root
               'tsconfig{.eslint.json,.json}',
               // look in dirs like node/react
-              // TODO: can these negations be smarter?
               '*/tsconfig{.eslint.json,.json}',
               // look in dirs like packages/package/*
               '*/*/tsconfig{.eslint.json,.json}',
@@ -30,102 +29,18 @@ module.exports = !hasTypescript
             projectFolderIgnoreList: ['node_modules/**/*'],
           },
           rules: {
-            // Enforce explicit accessibility modifiers on class properties and methods
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-member-accessibility.md
-            '@typescript-eslint/explicit-member-accessibility': [
-              'error',
-              {
-                accessibility: 'explicit',
-                overrides: {
-                  accessors: 'explicit',
-                  constructors: 'no-public',
-                  methods: 'explicit',
-                  parameterProperties: 'explicit',
-                },
-              },
-            ],
-
-            // Enforce explicit function return type
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
-            '@typescript-eslint/explicit-function-return-type': 'off',
-
-            // Enforce a consistent way of typing arrays
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/array-type.mdv
-            '@typescript-eslint/array-type': [
-              'warn',
-              {
-                default: 'array-simple',
-                readonly: 'array-simple',
-              },
-            ],
-
-            // Enforce a consitent way to type objects
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/consistent-type-definitions.md
-            '@typescript-eslint/consistent-type-definitions': 'off',
-
-            // Disallow non null assertions (!), comes from the recommended config
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-non-null-assertion.md
-            '@typescript-eslint/no-non-null-assertion': 'warn',
-
-            // Enforce that when adding two variables, operands must both be of type number or of type string
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-plus-operands.md
-            '@typescript-eslint/restrict-plus-operands': [
-              'error',
-              {
-                checkCompoundAssignments: true,
-              },
-            ],
-
-            // Enforce optional chaining over chaining AND (&&) operators
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-optional-chain.md
-            '@typescript-eslint/prefer-optional-chain': 'warn',
-
-            // Enforce optional chaining over chaining AND (&&) operators
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-non-null-asserted-optional-chain.md
-            '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-
-            // Enforce nullish coalescing over short-circuiting
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-nullish-coalescing.md
-            '@typescript-eslint/prefer-nullish-coalescing': [
-              'warn',
-              {
-                ignoreConditionalTests: true,
-                ignoreMixedLogicalExpressions: true,
-                forceSuggestionFixer: false,
-              },
-            ],
-
-            // Prefer usage of as const over literal type
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-as-const.md
-            // TODO: turn it on when 2.18.x is out
-            // '@typescript-eslint/prefer-as-const': 'error',
-
-            // Prevent unnecessary type arguments
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unnecessary-type-arguments.md
-            '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
-
-            // Warns when a namespace qualifier is unnecessary
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unnecessary-qualifier.md
-            '@typescript-eslint/no-unnecessary-qualifier': 'warn',
-
-            // Disallow throwing literals as exceptions
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-throw-literal.md
-            '@typescript-eslint/no-throw-literal': 'warn',
-
-            // Disallows invocation of require() in favor of import statements
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-require-imports.md
-            '@typescript-eslint/no-require-imports': 'warn',
-
-            // Disallows the use of eval()-like methods
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-implied-eval.md
-            '@typescript-eslint/no-implied-eval': 'error',
-
-            // Requires Array#sort calls to always provide a compareFunction
-            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/require-array-sort-compare.md
-            '@typescript-eslint/require-array-sort-compare': 'error',
-
             //! extensions of native eslint rules
             //! when modifying a rule here, make sure to modify the native one and vice-versa
+
+            // Disallow declaration of variables already declared in the outer scope
+            // https://eslint.org/docs/rules/no-shadow
+            'no-shadow': 'off',
+            '@typescript-eslint/no-shadow': [
+              'error',
+              {
+                allow: ['done', 'next', 'resolve', 'reject', 'cb'],
+              },
+            ],
 
             // Prevent unused declared variables
             // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
@@ -225,6 +140,100 @@ module.exports = !hasTypescript
                 typedefs: false,
               },
             ],
+            // ! ts only rules
+            // Enforce explicit accessibility modifiers on class properties and methods
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-member-accessibility.md
+            '@typescript-eslint/explicit-member-accessibility': [
+              'error',
+              {
+                accessibility: 'explicit',
+                overrides: {
+                  accessors: 'explicit',
+                  constructors: 'no-public',
+                  methods: 'explicit',
+                  parameterProperties: 'explicit',
+                },
+              },
+            ],
+
+            // Enforce explicit function return type
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-function-return-type.md
+            '@typescript-eslint/explicit-function-return-type': 'off',
+
+            // Enforce a consistent way of typing arrays
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/array-type.mdv
+            '@typescript-eslint/array-type': [
+              'warn',
+              {
+                default: 'array-simple',
+                readonly: 'array-simple',
+              },
+            ],
+
+            // Enforce a consitent way to type objects
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/consistent-type-definitions.md
+            '@typescript-eslint/consistent-type-definitions': 'off',
+
+            // Disallow non null assertions (!), comes from the recommended config
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-non-null-assertion.md
+            '@typescript-eslint/no-non-null-assertion': 'warn',
+
+            // Enforce that when adding two variables, operands must both be of type number or of type string
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/restrict-plus-operands.md
+            '@typescript-eslint/restrict-plus-operands': [
+              'error',
+              {
+                checkCompoundAssignments: true,
+              },
+            ],
+
+            // Enforce optional chaining over chaining AND (&&) operators
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-optional-chain.md
+            '@typescript-eslint/prefer-optional-chain': 'warn',
+
+            // Enforce optional chaining over chaining AND (&&) operators
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-non-null-asserted-optional-chain.md
+            '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+
+            // Enforce nullish coalescing over short-circuiting
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-nullish-coalescing.md
+            '@typescript-eslint/prefer-nullish-coalescing': [
+              'warn',
+              {
+                ignoreConditionalTests: true,
+                ignoreMixedLogicalExpressions: true,
+                forceSuggestionFixer: false,
+              },
+            ],
+
+            // Prefer usage of as const over literal type
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-as-const.md
+            // TODO: turn it on when 2.18.x is out
+            // '@typescript-eslint/prefer-as-const': 'error',
+
+            // Prevent unnecessary type arguments
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unnecessary-type-arguments.md
+            '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
+
+            // Warns when a namespace qualifier is unnecessary
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unnecessary-qualifier.md
+            '@typescript-eslint/no-unnecessary-qualifier': 'warn',
+
+            // Disallow throwing literals as exceptions
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-throw-literal.md
+            '@typescript-eslint/no-throw-literal': 'warn',
+
+            // Disallows invocation of require() in favor of import statements
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-require-imports.md
+            '@typescript-eslint/no-require-imports': 'warn',
+
+            // Disallows the use of eval()-like methods
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-implied-eval.md
+            '@typescript-eslint/no-implied-eval': 'error',
+
+            // Requires Array#sort calls to always provide a compareFunction
+            // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/require-array-sort-compare.md
+            '@typescript-eslint/require-array-sort-compare': 'error',
 
             // Enforce explicit enum item values
             // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-enum-initializers.md
