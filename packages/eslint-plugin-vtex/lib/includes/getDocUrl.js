76b0a628ca8ca95795ef5105de4a4e24cddafe44
Christian Kaisermann <christian@kaisermann.me>
2020-12-09 10:36:53 -0300

    Move master to main branch

--- packages/eslint-plugin-vtex/lib/includes/getDocUrl.js
+++ packages/eslint-plugin-vtex/lib/includes/getDocUrl.js
@@ -1,7 +1,7 @@
 const REPO_URL = 'https://github.com/vtex/javascript'
 
 function getDocUrl(ruleName) {
-  return `${REPO_URL}/blob/master/packages/eslint-plugin-vtex/docs/rules/${ruleName}.md`
+  return `${REPO_URL}/blob/main/packages/eslint-plugin-vtex/docs/rules/${ruleName}.md`
 }
 
 module.exports = {
