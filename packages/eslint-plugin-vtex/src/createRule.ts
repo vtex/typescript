cee7fd527a04d56a147a49badba05f77da928916
gtkatakura <gt.katakura@gmail.com>
2024-04-29 16:15:13 -0300

    chore(eslint-plugin-vtex): right docs url for eslint rules

--- packages/eslint-plugin-vtex/src/createRule.ts
+++ packages/eslint-plugin-vtex/src/createRule.ts
@@ -2,5 +2,5 @@ import { ESLintUtils } from '@typescript-eslint/utils'
 
 export const createRule = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github.com/vtex/typescript/tree/main/packages/eslint-plugin-vtex/docs/${name}.md`
+    `https://github.com/vtex/typescript/tree/main/packages/eslint-plugin-vtex/docs/rules/${name}.md`
 )
